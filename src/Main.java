0d4e7ba3fedff2ff1d3a6b1cd8c24f4597a097fb
herreang000 <herreang000@warren.k12.in.us>
2024-08-28 11:58:26 -0400

    final

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(printEqual(1, 1, 1));
+        System.out.println(printEqual(1, 1, 2));
+        System.out.println(printEqual(-1, -1, -1));
+        System.out.println(printEqual(1, 2, 3));
+    }
+
+
+    public static boolean printEqual(int a, int b, int c){
+        if(a <0 && c < 0 && c <0){
+            System.out.println("Invalid");
+        }else if (a == b && b == c){
+            System.out.println("All numbers are equal");
+        }else if (a != b && b != c){
+            System.out.println("All numbers are different");
+        }else{
+            System.out.println("Invalid");
+        };
+        return false;
+    }
 }
